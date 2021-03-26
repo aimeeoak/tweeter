a22251be7542979f8243f5b8a9d5704f1414ea99
Aimee <aimeegarriock@gmail.com>
2021-03-26 02:25:11 +0000

    tweeter form validation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,29 +9,35 @@ $(document).ready(function(){
   $('form').submit(function(event){
     event.preventDefault();
     console.log($(tweet).serialize());
+    console.log($("#tweet-text").val().length);
+    if ($("#tweet-text").val().length > 140) {
+      $('#error-message').text(errMessage("too long")).slideDown();
+    } else if ($("#tweet-text").val().length === null || $("#tweet-text").val().length === 0) {
+      $('#error-message').text(errMessage("null")).slideDown();
   });
 });
+
 
 const data = [
         {
           "user": {
-            "name": "Lucy Westenra",
-            "avatars": "https://i.imgur.com/73hZDYK.png",
-            "handle": "@theBlooferLady"
+            "name": "Leo Birdman",
+            "avatars": "",
+            "handle": "@PurrsianPeacock"
           },
           "content": {
-            "text": "Dress is a bore."
+            "text": "Any cool chicks looking to hang out with a guy with an excellent mane?"
           },
           "created_at": 1616480346544
         },
         {
           "user": {
-            "name": "Mina Harker",
-            "avatars": "https://i.imgur.com/nlhLi3I.png",
-            "handle": "@mrsharker"
+            "name": "Fluffy Feathers",
+            "avatars": "",
+            "handle": "@QueenFisher"
           },
           "content": {
-            "text": "Fortunately I am not of a fainting disposition"
+            "text": "If you want some bread, meet me under the bird feeder when my humans get home"
           },
           "created_at": 1616566746544
         }
@@ -53,17 +59,18 @@ const renderTweets = function(tweets) {
 const createTweetElement = function(tweet) {
   let $tweet = $(`
     <article class="tweet">
-      <header class="tweet-header">
+      <header class="tweet-header"> 
+      <span class="tweet-author">
+      <article class="tweet1">
+      <h1>${tweet.user.handle}</h1>
+      </article>
         <img src="${tweet.user.avatars}">
         <h1>${tweet.user.name}</h1>
         <article class="tweet">
           <h2>${tweet.content.text}</h2>
         </article>
-        <article class="tweet1">
-          <h1>${tweet.user.handle}</h1>
           <article class="tweet">
           </article>
-        </article>
     </header>
     <footer class="tweet-footer">
     ${tweet.created_at}
@@ -74,3 +81,12 @@ const createTweetElement = function(tweet) {
 };
 
 renderTweets(data);
+
+const error = function(errorMessage) {
+  let $error = ``;
+  if (errorMessage === "character limit") {
+    $error = "Talk less";
+  } else if (errorMessage === null) {
+    $error = "Talk more";
+  } return $error;
+};
