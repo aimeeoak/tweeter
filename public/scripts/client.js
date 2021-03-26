5285c0caec61d30bc4d6760553fd534bc2fd1332
Aimee <aimeegarriock@gmail.com>
2021-03-26 05:54:14 +0000

    updated aesthetics

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,32 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    "user": {
+      "name": "Leo Birdman",
+      "avatars": "",
+      "handle": "@PurrsianPeacock"
+    },
+    "content": {
+      "text": "Any cool chicks looking to hang out with a guy with an excellent mane?"
+    },
+    "created_at": 1616480346544
+  },
+  {
+    "user": {
+      "name": "Fluffy Feathers",
+      "avatars": "",
+      "handle": "@QueenFisher"
+    },
+    "content": {
+      "text": "If you want some bread, meet me under the bird feeder when my humans get home"
+    },
+    "created_at": 1616566746544
+  }
+]
+
+
 $(document).ready(function(){
   renderTweets(data);
   $('form').submit(function(event){
@@ -13,35 +39,10 @@ $(document).ready(function(){
     if ($("#tweet-text").val().length > 140) {
       $('#error-message').text(errMessage("too long")).slideDown();
     } else if ($("#tweet-text").val().length === null || $("#tweet-text").val().length === 0) {
-      $('#error-message').text(errMessage("null")).slideDown();
-  });
+      $('#error-message').text(errMessage("null")).slideDown();form 
+  };
+})
 });
-
-
-const data = [
-        {
-          "user": {
-            "name": "Leo Birdman",
-            "avatars": "",
-            "handle": "@PurrsianPeacock"
-          },
-          "content": {
-            "text": "Any cool chicks looking to hang out with a guy with an excellent mane?"
-          },
-          "created_at": 1616480346544
-        },
-        {
-          "user": {
-            "name": "Fluffy Feathers",
-            "avatars": "",
-            "handle": "@QueenFisher"
-          },
-          "content": {
-            "text": "If you want some bread, meet me under the bird feeder when my humans get home"
-          },
-          "created_at": 1616566746544
-        }
-      ]
 
 const loadTweets = function() {
   $.ajax( "/tweets", { method: "GET"})
@@ -50,6 +51,8 @@ const loadTweets = function() {
       console.log("hello... it's tweets...", tweets)
     });
 };
+
+loadTweets(); 
 
 const renderTweets = function(tweets) {
   for (let tweet of tweets)
@@ -60,17 +63,13 @@ const createTweetElement = function(tweet) {
   let $tweet = $(`
     <article class="tweet">
       <header class="tweet-header"> 
-      <span class="tweet-author">
-      <article class="tweet1">
-      <h1>${tweet.user.handle}</h1>
-      </article>
+      <article class="tweet-author">
+      <b>${tweet.user.name}</b>
+      <span class="tweet-handle">${tweet.user.handle}</span>
         <img src="${tweet.user.avatars}">
-        <h1>${tweet.user.name}</h1>
-        <article class="tweet">
-          <h2>${tweet.content.text}</h2>
+        <article class="tweet-tweet">
+          ${tweet.content.text}
         </article>
-          <article class="tweet">
-          </article>
     </header>
     <footer class="tweet-footer">
     ${tweet.created_at}
@@ -89,4 +88,4 @@ const error = function(errorMessage) {
   } else if (errorMessage === null) {
     $error = "Talk more";
   } return $error;
-};
+}
